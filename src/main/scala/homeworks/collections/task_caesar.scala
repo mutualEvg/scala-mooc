1210ee0782b34418cac08f8c69b575a5dfdee86b
Evgeny Losev <e.losev@altuera.com>
2021-04-28 10:37:01 -0400

    first commit

---
+++ src/main/scala/homeworks/collections/task_caesar.scala
@@ -0,0 +1,53 @@
+package homeworks.collections
+
+import homeworks.HomeworksUtils.TaskSyntax
+
+object task_caesar {
+
+  /**
+   * В данном задании Вам предлагается реализовать функции,
+   * реализующие кодирование/декодирование строки шифром Цезаря.
+   * https://ru.wikipedia.org/wiki/Шифр_Цезаря
+   * Алфавит - прописные латинские буквы от A до Z.
+   * Сдвиг   - неотрицательное целое число.
+   * Пример: при сдвиге 2 слово "SCALA" шифруется как "UECNC".
+   */
+  val upper = 'A' to 'Z'
+  val indexedUpper = upper.zipWithIndex.toMap.map(entry => (entry._1, entry._2 + 1))
+  val swapIndexedUpper = indexedUpper.map(_.swap)
+  /**
+   * @param word   входное слово, которое необходимо зашифровать
+   * @param offset сдвиг вперёд по алфавиту
+   * @return зашифрованное слово
+   */
+  def encrypt(word: String, offset: Int): String =
+    word.map(string => getValue(indexedUpper(string), offset)).map(number => swapIndexedUpper(number)).mkString
+
+  /**
+   * @param cipher шифр, который необходимо расшифровать
+   * @param offset сдвиг вперёд по алфавиту
+   * @return расшифрованное слово
+   */
+  def decrypt(cipher: String, offset: Int): String =
+    cipher.map(string => getDecriptedValue(indexedUpper(string), offset)).map(number => swapIndexedUpper(number)).mkString
+
+  def getValue(strindex: Int, offset: Int): Int = {
+    if (offset > 26 && (strindex + (offset%26)) > 26){
+      (strindex + (offset%26))%26
+    } else if (offset > 26 && (strindex + (offset%26)) <= 26) {
+      strindex + (offset%26)
+    } else if (offset < 26 && (strindex + (offset%26)) > 26) {
+      strindex + offset - 26
+    } else strindex + offset
+  }
+  def getDecriptedValue(strindex: Int, offset: Int): Int =
+    if (offset > 26) {
+      val x = offset-offset/26*26
+      if (strindex - x < 1){
+        strindex - x + 26 }
+      else strindex - x
+    } else {
+      if (strindex - offset < 1){ strindex - offset + 26 }
+      else strindex - offset
+    }
+}
