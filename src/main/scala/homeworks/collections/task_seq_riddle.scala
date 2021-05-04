b190356f2a272d6b999d14ce51da8a2550a6dc49
Evgeny Losev <e.losev@altuera.com>
2021-05-04 11:43:21 -0400

    seq hw

--- src/main/scala/homeworks/collections/task_seq_riddle.scala
+++ src/main/scala/homeworks/collections/task_seq_riddle.scala
@@ -18,8 +18,27 @@ object task_seq_riddle {
    * 1. Реализуйте функцию генерирующую след последовательность из текущей
    * */
 
-  def nextLine(currentLine: List[Int]): List[Int] =
-    task"Реализуйте функцию генерирующую след последовательность из текущей"()
+  def nextLine(currentLine: List[Int]): List[Int] = currentLine
+    .foldLeft(List.empty[Count])(foldElements)
+    .reverse
+    .flatMap(a => a.flatten)
+
+  def foldElements(list: List[Count], digit: Int): List[Count] = {
+    if (list.isEmpty) {
+      List(Count(digit))
+    } else {
+      if (list.head.digit == digit) {
+        list.head.increment :: list.tail
+      } else {
+        Count(digit) :: list
+      }
+    }
+  }
+
+  case class Count(digit: Int, count: Int = 1) {
+    def increment: Count = copy(count = count + 1)
+    def flatten: List[Int] = List(count, digit)
+  }
 
   /**
    * 2. Реализуйте ленивый список, который генерирует данную последовательность
@@ -29,6 +48,5 @@ object task_seq_riddle {
    *
    */
 
-  val funSeq: LazyList[List[Int]] =
-    task"Реализуйте ленивый список, который генерирует данную последовательность"()
+  val funSeq: LazyList[List[Int]] = LazyList.iterate(List(1))(nextLine)
 }
