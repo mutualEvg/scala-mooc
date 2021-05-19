46e6f65689a7a27aa639072b8b0d78bf3f957c94
Evgeny Losev <e.losev@altuera.com>
2021-05-19 16:43:29 -0400

    caesar 2

--- src/main/scala/homeworks/collections/task_caesar.scala
+++ src/main/scala/homeworks/collections/task_caesar.scala
@@ -51,3 +51,40 @@ object task_caesar {
       else strindex - offset
     }
 }
+object task_caesar2 {
+
+  private val ALPHABET: IndexedSeq[Char] = 'A' to 'Z'
+
+  /**
+   * В данном задании Вам предлагается реализовать функции,
+   * реализующие кодирование/декодирование строки шифром Цезаря.
+   * https://ru.wikipedia.org/wiki/Шифр_Цезаря
+   * Алфавит - прописные латинские буквы от A до Z.
+   * Сдвиг   - неотрицательное целое число.
+   * Пример: при сдвиге 2 слово "SCALA" шифруется как "UECNC".
+   */
+  /**
+   * @param word   входное слово, которое необходимо зашифровать
+   * @param offset сдвиг вперёд по алфавиту
+   * @return зашифрованное слово
+   */
+  def encrypt(word: String, offset: Int): String = word.map(encryptChar(offset))
+
+  /**
+   * @param cipher шифр, который необходимо расшифровать
+   * @param offset сдвиг вперёд по алфавиту
+   * @return расшифрованное слово
+   */
+  def decrypt(cipher: String, offset: Int): String = encrypt(cipher, -offset)
+
+  private def encryptChar(offset: Int)(char: Char): Char = {
+    val index = calculateIndex(char, offset)
+    if (index >= 0) {
+      ALPHABET(index)
+    } else {
+      ALPHABET(index + ALPHABET.size)
+    }
+  }
+
+  private def calculateIndex(char: Char, offset: Int) = (ALPHABET.indexOf(char) + offset) % ALPHABET.size
+}
